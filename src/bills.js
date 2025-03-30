74c62020cd85f3fbf784f6bc3e9ac19326c310a9
M-Ayushi <ayushimahajan05@gmail.com>
2025-03-30 14:57:18 +0100

    Update, Edit and Delete Bills

--- src/bills.js
+++ src/bills.js
@@ -39,7 +39,6 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var express = require("express");
 var mongoose_1 = require("mongoose");
 var item_1 = require("./models/item");
-//import cron from 'node-cron';
 var cron = require("node-cron");
 // import User from './models/User';
 // import House from '.models/house';
@@ -47,8 +46,8 @@ var uri = "mongodb+srv://aleenashaiju01:HTHxjwKWgWKjD2Y5@bills.jtyzd.mongodb.net
 var index = express();
 index.use(express.json());
 var payee = new mongoose_1.default.Types.ObjectId('67bf910446216131dd018d88');
-//to test if finding/displaying the bills the user needs to pay are working
-var user = new mongoose_1.default.Types.ObjectId('67bf910446216131dd018d14');
+// to test if finding/displaying the bills the user needs to pay are working
+// const user = new mongoose.Types.ObjectId('67bf910446216131dd018d14');
 var generateNextDate = function (currentDate, recurringType) {
     var nextDate = new Date(currentDate);
     if (recurringType === 'Weekly') {
@@ -62,6 +61,7 @@ var generateNextDate = function (currentDate, recurringType) {
     }
     return nextDate;
 };
+// cron.schedule('0 0 * * *', async () => { /* Runs at midnight daily */ });
 cron.schedule('* * * * *', function () { return __awaiter(void 0, void 0, void 0, function () {
     var today, overdueBills, _i, overdueBills_1, bill, newDueDate, existingBill, newBill, err_1;
     return __generator(this, function (_a) {
@@ -93,6 +93,7 @@ cron.schedule('* * * * *', function () { return __awaiter(void 0, void 0, void 0
             case 4:
                 existingBill = _a.sent();
                 if (existingBill) {
+                    console.log("No recurring bills need to be created today.");
                     return [3 /*break*/, 6];
                 }
                 newBill = new item_1.default({
@@ -184,13 +185,18 @@ index.get('/all-bill', function (req, res) { return __awaiter(void 0, void 0, vo
     });
 }); });
 index.get('/bills-to-pay', function (req, res) { return __awaiter(void 0, void 0, void 0, function () {
-    var billsToPay, err_4;
+    var userId, billsToPay, err_4;
     return __generator(this, function (_a) {
         switch (_a.label) {
             case 0:
                 _a.trys.push([0, 2, , 3]);
-                console.log("Finding bills where ", user, " is a payor.");
-                return [4 /*yield*/, item_1.default.find({ "Payors.payorId": user })];
+                userId = req.query.userId;
+                if (!userId) {
+                    res.status(400).json({ error: "Missing userId query parameter." });
+                    return [2 /*return*/];
+                }
+                console.log("Finding bills where ", userId, " is a payor.");
+                return [4 /*yield*/, item_1.default.find({ "Payors.payorId": userId }).sort({ Deadline: 1 })];
             case 1:
                 billsToPay = _a.sent();
                 res.json(billsToPay);
@@ -205,28 +211,188 @@ index.get('/bills-to-pay', function (req, res) { return __awaiter(void 0, void 0
     });
 }); });
 index.get('/bills-owed', function (req, res) { return __awaiter(void 0, void 0, void 0, function () {
-    var bills, unpaidBills, paidBills, err_5;
+    var userId, bills, unpaidBills, paidBills, err_5;
     return __generator(this, function (_a) {
         switch (_a.label) {
             case 0:
                 _a.trys.push([0, 2, , 3]);
-                console.log("Finding bills where ", user, " is a payee.");
-                return [4 /*yield*/, item_1.default.find({ Payee: user })];
+                userId = req.query.userId;
+                if (!userId) {
+                    res.status(400).json({ error: "Missing userId query parameter." });
+                    return [2 /*return*/];
+                }
+                console.log("Finding bills where ", userId, " is a payee.");
+                return [4 /*yield*/, item_1.default.find({ Payee: userId }).sort({ Deadline: 1 })];
             case 1:
                 bills = _a.sent();
-                unpaidBills = bills.filter(function (bill) { return bill.Payors.some(function (payor) { return payor.status == "Unpaid"; }); });
-                paidBills = bills.filter(function (bill) { return bill.Payors.some(function (payors) { return payors.status == "Paid"; }); });
+                unpaidBills = bills.filter(function (bill) { return bill.Payors.some(function (payor) { return payor.status === "Unpaid"; }); });
+                paidBills = bills.filter(function (bill) { return bill.Payors.some(function (payor) { return payor.status === "Paid"; }); });
                 res.json({
-                    "You are owed: ": unpaidBills,
-                    "Please confirm you have received: ": paidBills,
+                    "You are owed": unpaidBills,
+                    "Please confirm you have received": paidBills,
                 });
                 return [3 /*break*/, 3];
             case 2:
                 err_5 = _a.sent();
                 console.error(err_5);
-                res.status(500).json({ error: 'Could not fetch bills owed.' });
+                res.status(500).json({ error: "Could not fetch bills owed." });
                 return [3 /*break*/, 3];
             case 3: return [2 /*return*/];
+        }
+    });
+}); });
+// Update the bill status [Unpaid -> Paid -> Confirmed]
+index.put('/update-bill-status/:billId', function (req, res) { return __awaiter(void 0, void 0, void 0, function () {
+    var billId, _a, userId, payorId_1, newStatus, validStatuses, bill, payor, payor, err_6;
+    return __generator(this, function (_b) {
+        switch (_b.label) {
+            case 0:
+                _b.trys.push([0, 6, , 7]);
+                billId = req.params.billId;
+                _a = req.body, userId = _a.userId, payorId_1 = _a.payorId, newStatus = _a.newStatus;
+                validStatuses = ["Unpaid", "Paid", "Confirmed"];
+                if (!validStatuses.includes(newStatus)) {
+                    res.status(400).json({ error: "Invalid status. Choose 'Unpaid', 'Paid', or 'Confirmed'." });
+                    return [2 /*return*/];
+                }
+                return [4 /*yield*/, item_1.default.findById(billId)];
+            case 1:
+                bill = _b.sent();
+                if (!bill) {
+                    res.status(404).json({ error: "Bill not found." });
+                    return [2 /*return*/];
+                }
+                if (!(userId === payorId_1)) return [3 /*break*/, 3];
+                payor = bill.Payors.find(function (p) { return p.payorId.toString() === payorId_1; });
+                if (!payor) {
+                    res.status(404).json({ error: "Payor not found in this bill." });
+                    return [2 /*return*/];
+                }
+                if (payor.status !== "Unpaid") {
+                    res.status(400).json({ error: "You can only change your status from 'Unpaid' to 'Paid'." });
+                    return [2 /*return*/];
+                }
+                payor.status = "Paid"; // Update status
+                return [4 /*yield*/, bill.save()];
+            case 2:
+                _b.sent();
+                res.json({ message: "Payment marked as 'Paid'.", updatedBill: bill });
+                return [2 /*return*/];
+            case 3:
+                if (!(bill.Payee.toString() === userId)) return [3 /*break*/, 5];
+                payor = bill.Payors.find(function (p) { return p.payorId.toString() === payorId_1; });
+                if (!payor) {
+                    res.status(404).json({ error: "Payor not found in this bill." });
+                    return [2 /*return*/];
+                }
+                if (payor.status !== "Paid") {
+                    res.status(400).json({ error: "You can only change 'Paid' to 'Confirmed'." });
+                    return [2 /*return*/];
+                }
+                payor.status = "Confirmed"; // Update status
+                return [4 /*yield*/, bill.save()];
+            case 4:
+                _b.sent();
+                res.json({ message: "Payment confirmed.", updatedBill: bill });
+                return [2 /*return*/];
+            case 5:
+                // If neither a payor nor a payee, the user cannot update the status
+                res.status(403).json({ error: "You are not authorized to update this payment status." });
+                return [2 /*return*/];
+            case 6:
+                err_6 = _b.sent();
+                console.error(err_6);
+                res.status(500).json({ error: "An error occurred while updating payment status." });
+                return [3 /*break*/, 7];
+            case 7: return [2 /*return*/];
+        }
+    });
+}); });
+// Update a preexisting bill
+index.put('/edit-bill/:billId', function (req, res) { return __awaiter(void 0, void 0, void 0, function () {
+    var billId, _a, userId, Item, Amount, Deadline, Recurring, Payors, bill, updatedBill, err_7;
+    return __generator(this, function (_b) {
+        switch (_b.label) {
+            case 0:
+                _b.trys.push([0, 3, , 4]);
+                billId = req.params.billId;
+                _a = req.body, userId = _a.userId, Item = _a.Item, Amount = _a.Amount, Deadline = _a.Deadline, Recurring = _a.Recurring, Payors = _a.Payors;
+                return [4 /*yield*/, item_1.default.findById(billId)];
+            case 1:
+                bill = _b.sent();
+                if (!bill) {
+                    res.status(404).json({ error: "Bill not found." });
+                    return [2 /*return*/];
+                }
+                // Check if the user is the payee (only the creator can edit)
+                if (bill.Payee.toString() !== userId) {
+                    res.status(403).json({ error: "You are not authorized to edit this bill." });
+                    return [2 /*return*/];
+                }
+                // Update bill fields (only update provided values)
+                if (Item)
+                    bill.Item = Item;
+                if (Amount)
+                    bill.Amount = Amount;
+                if (Deadline)
+                    bill.Deadline = Deadline;
+                if (Recurring)
+                    bill.Recurring = Recurring;
+                // If Payors are provided, update them
+                if (Payors) {
+                    bill.Payors = Payors.map(function (payorId) { return ({
+                        payorId: payorId,
+                        status: 'Unpaid' // Reset statuses to unpaid
+                    }); });
+                }
+                return [4 /*yield*/, bill.save()];
+            case 2:
+                updatedBill = _b.sent();
+                res.json({ message: "Bill updated successfully.", updatedBill: updatedBill });
+                return [3 /*break*/, 4];
+            case 3:
+                err_7 = _b.sent();
+                console.error("Error updating bill:", err_7);
+                res.status(500).json({ error: "An error occurred while updating the bill." });
+                return [3 /*break*/, 4];
+            case 4: return [2 /*return*/];
+        }
+    });
+}); });
+// Delete a bill
+index.delete('/delete-bill/:billId', function (req, res) { return __awaiter(void 0, void 0, void 0, function () {
+    var billId, userId, bill, err_8;
+    return __generator(this, function (_a) {
+        switch (_a.label) {
+            case 0:
+                _a.trys.push([0, 3, , 4]);
+                billId = req.params.billId;
+                userId = req.body.userId;
+                return [4 /*yield*/, item_1.default.findById(billId)];
+            case 1:
+                bill = _a.sent();
+                if (!bill) {
+                    res.status(404).json({ error: "Bill not found." });
+                    return [2 /*return*/];
+                }
+                // Check if the user is the payee (only the creator can delete)
+                if (bill.Payee.toString() !== userId) {
+                    res.status(403).json({ error: "You are not authorized to delete this bill." });
+                    return [2 /*return*/];
+                }
+                // Delete the bill
+                return [4 /*yield*/, item_1.default.findByIdAndDelete(billId)];
+            case 2:
+                // Delete the bill
+                _a.sent();
+                res.json({ message: "Bill deleted successfully." });
+                return [3 /*break*/, 4];
+            case 3:
+                err_8 = _a.sent();
+                console.error("Error deleting bill:", err_8);
+                res.status(500).json({ error: "An error occurred while deleting the bill." });
+                return [3 /*break*/, 4];
+            case 4: return [2 /*return*/];
         }
     });
 }); });
@@ -237,10 +403,22 @@ mongoose_1.default.connect(uri)
 })
     .catch(function (err) { return console.log(err); });
 // curl -X POST http://localhost:3000/add-bill -H "Content-Type: application/json" -d "{\"Item\": \"Electricity\", \"Payee\": \"John\", \"Amount\": 50, \"Status\": \"Unpaid\", \"Deadline\": \"2025-03-10\", \"Recurring\": \"Monthly\", \"Payors\": [\"67bf910446216131dd018d14\", \"67bf910446216131dd018d56\"]}"
-// Status system
+// Test adding a bill
+// curl -X POST http://localhost:3000/add-bill -H "Content-Type: application/json" -d "{\"Item\": \"Sample Recurring Bill\", \"Amount\": 123, \"Status\": \"Unpaid\", \"Deadline\": \"2025-03-30\", \"Recurring\": \"Weekly\", \"Payors\": [\"67bf910446216131dd018d14\", \"67bf910446216131dd018d56\"]}"
+// ------test updating a bill-------
+// Payor marks own bill as paid
+// curl -X PUT "http://localhost:3000/update-bill-status/67e9346cb70ec50344644723" -H "Content-Type: application/json" -d "{\"userId\": \"67bf910446216131dd018d14\", \"payorId\": \"67bf910446216131dd018d14\", \"newStatus\": \"Paid\"}"
+// Payor tries to mark someone elses bill as paid (should fail)
+// Payor tries to mark alrd paid bill as paid (idk what should happen)
+// Payee marks bill as confirmed
+// Payor tries to makr bill as confirmed but payee is not valid (should fail)
+// user tries to mark bill as confirmed but is not tthe payor (should fail)
+// ---------------testing editing a bill ------------
+// curl -X PUT "http://localhost:3000/edit-bill/67e923b8f86bf385172ca729" -H "Content-Type: application/json" -d "{\"userId\": \"67bf910446216131dd018d88\", \"Item\": \"Updated Electricity Bill\", \"Amount\": 75, \"Deadline\": \"2025-04-15\", \"Recurring\": \"Biweekly\", \"Payors\": [\"67bf910446216131dd018d14\", \"67bf910446216131dd018d56\"]}"
+// Status system - nned to do the code so that if the user can change it from unpaid to paid to confirmed etc
 // Edit bills
 // Delete bills
-// Recurring bills
+// Recurring bills - DONE (?)
 // Getting all bills - only return relevant bills, ideally in order of deadlines
 // app.get('/get-email/:userID)
 // user should not need to type their own user id (remove payee like autofill) KINDA DONE
@@ -248,3 +426,5 @@ mongoose_1.default.connect(uri)
 // displaying all of current users unpaid bills DONE
 // display all bills with current user as payee DONE
 // displays bills where the current user needs to confirm receiving 
+// netstat -ano | findstr :3000
+// taskkill /PID [enter number] /F
--- src/bills.ts
+++ src/bills.ts
@@ -3,7 +3,6 @@ import express = require('express');
 import { Request, Response } from 'express';
 import mongoose from 'mongoose';
 import Bill from './models/item';
-//import cron from 'node-cron';
 import cron = require('node-cron');
 // import User from './models/User';
 // import House from '.models/house';
@@ -15,8 +14,8 @@ const index = express();
 index.use(express.json());
 const payee = new mongoose.Types.ObjectId('67bf910446216131dd018d88');
 
-//to test if finding/displaying the bills the user needs to pay are working
-const user = new mongoose.Types.ObjectId('67bf910446216131dd018d14');
+// to test if finding/displaying the bills the user needs to pay are working
+// const user = new mongoose.Types.ObjectId('67bf910446216131dd018d14');
 
 
 const generateNextDate = (currentDate: Date, recurringType: string): Date => {
@@ -33,6 +32,7 @@ const generateNextDate = (currentDate: Date, recurringType: string): Date => {
   return nextDate;
 };
 
+// cron.schedule('0 0 * * *', async () => { /* Runs at midnight daily */ });
 cron.schedule('* * * * *', async () => {
   console.log("Checking for recurring bills..");
 
@@ -146,39 +146,200 @@ index.get('/all-bill', async (req: Request, res: Response) => {
 
 index.get('/bills-to-pay', async (req: Request, res: Response) => {
   try {
-    console.log("Finding bills where ", user, " is a payor.");
+    const { userId } = req.query; // Use query parameter
 
-    //Find bills where the user is in the Payors list
-    const billsToPay = await Bill.find({ "Payors.payorId": user });
+    if (!userId) {
+      res.status(400).json({ error: "Missing userId query parameter." });
+      return;
+    }
+
+    console.log("Finding bills where ", userId, " is a payor.");
+
+    // Find bills where the user is in the Payors list and sort by deadline
+    const billsToPay = await Bill.find({ "Payors.payorId": userId }).sort({ Deadline: 1 });
 
     res.json(billsToPay);
-  } catch (err: any) {
+  } catch (err) {
     console.error(err);
     res.status(500).json({ error: 'Could not fetch bills to pay.' });
   }
 });
 
+
 index.get('/bills-owed', async (req: Request, res: Response) => {
   try {
-    console.log("Finding bills where ", user, " is a payee.");
+    const { userId } = req.query; // Use query parameters
 
-    //Find bills where the user is owed money
-    const bills = await Bill.find({ Payee: user });
+    if (!userId) {
+      res.status(400).json({ error: "Missing userId query parameter." });
+      return;
+    }
 
-    const unpaidBills = bills.filter(bill => bill.Payors.some(payor => payor.status == "Unpaid"));
+    console.log("Finding bills where ", userId, " is a payee.");
 
-    const paidBills = bills.filter(bill => bill.Payors.some(payors => payors.status == "Paid"));
+    // Find bills where the user is owed money, sorted by deadline
+    const bills = await Bill.find({ Payee: userId }).sort({ Deadline: 1 });
 
+    // Separate unpaid and paid bills
+    const unpaidBills = bills.filter(bill => bill.Payors.some(payor => payor.status === "Unpaid"));
+    const paidBills = bills.filter(bill => bill.Payors.some(payor => payor.status === "Paid"));
 
     res.json({
-      "You are owed: ": unpaidBills,
-      "Please confirm you have received: ":  paidBills,
+      "You are owed": unpaidBills,
+      "Please confirm you have received": paidBills,
     });
-  } catch (err: any) {
+  } catch (err) {
     console.error(err);
-    res.status(500).json({ error: 'Could not fetch bills owed.' });
+    res.status(500).json({ error: "Could not fetch bills owed." });
   }
 });
+
+
+// Update the bill status [Unpaid -> Paid -> Confirmed]
+
+index.put('/update-bill-status/:billId', async (req: Request, res: Response) => {
+  try {
+    const { billId } = req.params;
+    const { userId, payorId, newStatus } = req.body; // userId = the user making the request
+
+    // Validate new status
+    const validStatuses = ["Unpaid", "Paid", "Confirmed"];
+    if (!validStatuses.includes(newStatus)) {
+      res.status(400).json({ error: "Invalid status. Choose 'Unpaid', 'Paid', or 'Confirmed'." });
+      return;
+    }
+
+    // Find the bill
+    const bill = await Bill.findById(billId);
+    if (!bill) {
+      res.status(404).json({ error: "Bill not found." });
+      return;
+    }
+
+    // If the user is a payor, they can only mark their own status as "Paid"
+    if (userId === payorId) {
+      const payor = bill.Payors.find(p => p.payorId.toString() === payorId);
+      if (!payor) {
+        res.status(404).json({ error: "Payor not found in this bill." });
+        return;
+      }
+
+      if (payor.status !== "Unpaid") {
+        res.status(400).json({ error: "You can only change your status from 'Unpaid' to 'Paid'." });
+        return;
+      }
+
+      payor.status = "Paid"; // Update status
+      await bill.save();
+      res.json({ message: "Payment marked as 'Paid'.", updatedBill: bill });
+      return;
+    }
+
+    // If the user is the payee (bill owner), they can change "Paid" to "Confirmed"
+    if (bill.Payee.toString() === userId) {
+      const payor = bill.Payors.find(p => p.payorId.toString() === payorId);
+      if (!payor) {
+        res.status(404).json({ error: "Payor not found in this bill." });
+        return;
+      }
+
+      if (payor.status !== "Paid") {
+        res.status(400).json({ error: "You can only change 'Paid' to 'Confirmed'." });
+        return;
+      }
+
+      payor.status = "Confirmed"; // Update status
+      await bill.save();
+      res.json({ message: "Payment confirmed.", updatedBill: bill });
+      return;
+    }
+
+    // If neither a payor nor a payee, the user cannot update the status
+    res.status(403).json({ error: "You are not authorized to update this payment status." });
+    return;
+
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "An error occurred while updating payment status." });
+  }
+});
+
+// Update a preexisting bill
+index.put('/edit-bill/:billId', async (req: Request, res: Response) => {
+  try {
+    const { billId } = req.params;
+    const { userId, Item, Amount, Deadline, Recurring, Payors } = req.body;
+
+    // Find the bill by ID
+    const bill = await Bill.findById(billId);
+
+    if (!bill) {
+      res.status(404).json({ error: "Bill not found." });
+      return;
+    }
+
+    // Check if the user is the payee (only the creator can edit)
+    if (bill.Payee.toString() !== userId) {
+      res.status(403).json({ error: "You are not authorized to edit this bill." });
+      return;
+    }
+
+    // Update bill fields (only update provided values)
+    if (Item) bill.Item = Item;
+    if (Amount) bill.Amount = Amount;
+    if (Deadline) bill.Deadline = Deadline;
+    if (Recurring) bill.Recurring = Recurring;
+    
+    // If Payors are provided, update them
+    if (Payors) {
+      bill.Payors = Payors.map((payorId: string) => ({
+        payorId,
+        status: 'Unpaid' // Reset statuses to unpaid
+      }));
+    }
+
+    // Save the updated bill
+    const updatedBill = await bill.save();
+
+    res.json({ message: "Bill updated successfully.", updatedBill });
+
+  } catch (err) {
+    console.error("Error updating bill:", err);
+    res.status(500).json({ error: "An error occurred while updating the bill." });
+  }
+});
+
+// Delete a bill
+index.delete('/delete-bill/:billId', async (req: Request, res: Response) => {
+  try {
+    const { billId } = req.params;
+    const { userId } = req.body; // User making the request
+
+    // Find the bill by ID
+    const bill = await Bill.findById(billId);
+
+    if (!bill) {
+      res.status(404).json({ error: "Bill not found." });
+      return;
+    }
+
+    // Check if the user is the payee (only the creator can delete)
+    if (bill.Payee.toString() !== userId) {
+      res.status(403).json({ error: "You are not authorized to delete this bill." });
+      return;
+    }
+
+    // Delete the bill
+    await Bill.findByIdAndDelete(billId);
+
+    res.json({ message: "Bill deleted successfully." });
+
+  } catch (err) {
+    console.error("Error deleting bill:", err);
+    res.status(500).json({ error: "An error occurred while deleting the bill." });
+  }
+});
+
 
 mongoose.connect(uri)
   .then((result) => {
@@ -189,6 +350,23 @@ mongoose.connect(uri)
 
 
 // curl -X POST http://localhost:3000/add-bill -H "Content-Type: application/json" -d "{\"Item\": \"Electricity\", \"Payee\": \"John\", \"Amount\": 50, \"Status\": \"Unpaid\", \"Deadline\": \"2025-03-10\", \"Recurring\": \"Monthly\", \"Payors\": [\"67bf910446216131dd018d14\", \"67bf910446216131dd018d56\"]}"
+
+// Test adding a bill
+// curl -X POST http://localhost:3000/add-bill -H "Content-Type: application/json" -d "{\"Item\": \"Sample Recurring Bill\", \"Amount\": 123, \"Status\": \"Unpaid\", \"Deadline\": \"2025-03-30\", \"Recurring\": \"Weekly\", \"Payors\": [\"67bf910446216131dd018d14\", \"67bf910446216131dd018d56\"]}"
+
+// ------test updating a bill-------
+// Payor marks own bill as paid
+// curl -X PUT "http://localhost:3000/update-bill-status/67e9346cb70ec50344644723" -H "Content-Type: application/json" -d "{\"userId\": \"67bf910446216131dd018d14\", \"payorId\": \"67bf910446216131dd018d14\", \"newStatus\": \"Paid\"}"
+
+// Payor tries to mark someone elses bill as paid (should fail)
+// Payor tries to mark alrd paid bill as paid (idk what should happen)
+// Payee marks bill as confirmed
+// Payor tries to makr bill as confirmed but payee is not valid (should fail)
+// user tries to mark bill as confirmed but is not tthe payor (should fail)
+
+// ---------------testing editing a bill ------------
+// curl -X PUT "http://localhost:3000/edit-bill/67e923b8f86bf385172ca729" -H "Content-Type: application/json" -d "{\"userId\": \"67bf910446216131dd018d88\", \"Item\": \"Updated Electricity Bill\", \"Amount\": 75, \"Deadline\": \"2025-04-15\", \"Recurring\": \"Biweekly\", \"Payors\": [\"67bf910446216131dd018d14\", \"67bf910446216131dd018d56\"]}"
+
 
 // Status system - nned to do the code so that if the user can change it from unpaid to paid to confirmed etc
 // Edit bills
